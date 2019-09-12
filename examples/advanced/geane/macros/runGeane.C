d516b12ea9c7b65f8963b006836b288aa8f45cb7
Dennis Klein <d.klein@gsi.de>
2019-09-12 18:18:38 +0200

    Example.Geane: Cleanup Geane macro
    * Clarify ownership (some memory still leaks, but this needs fixes on
        framework side, see #923)
    * Apply consistent variable naming (camel-case, start with small letters)
    * Improve comments
    * Apply consistent indentation and spacing

--- examples/advanced/geane/macros/runGeane.C
+++ examples/advanced/geane/macros/runGeane.C
@@ -5,89 +5,79 @@
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
+
+#include <memory>
+
 int runGeane()
 {
+  // -----   Parameters   ---------------------------------------------------
+  TString dir = getenv("VMCWORKDIR");
 
-    TString dir = getenv("VMCWORKDIR");
-    TString tutdir = dir + "/simulation/geane";
+  TString tutGeomDir = dir + "/common/geometry";
+  gSystem->Setenv("GEOMPATH", tutGeomDir.Data());
 
-    TString tut_geomdir = dir + "/common/geometry";
-    gSystem->Setenv("GEOMPATH",tut_geomdir.Data());
+  TString tutConfigDir = dir + "/common/gconfig";
+  gSystem->Setenv("CONFIG_DIR", tutConfigDir.Data());
 
-    TString tut_configdir = dir + "/common/gconfig";
-    gSystem->Setenv("CONFIG_DIR",tut_configdir.Data());
+  TString inFile = "geane.mc.root"; // Input file (MC events)
+  TString parFile = "geane.par.root"; // Parameter file
+  TString outFile = "geane.cal.root"; // Output file
 
-    // Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
-    Int_t iVerbose = 0; // just forget about it, for the moment
+  // -----   Create run   ---------------------------------------------------
+  FairRunAna* run = new FairRunAna(); // singleton! FIXME
+  // FairRunAna* run = FairRunAna::Instance(); // should be this
+  run->SetSource(new FairFileSource  (inFile));
+  run->SetSink  (new FairRootFileSink(outFile));
 
-    // Input file (MC events)
-    TString inFile = "geane.mc.root";
+  FairRuntimeDb* rtdb = run->GetRuntimeDb();
 
-    // Parameter file
-    TString parFile = "geane.par.root";
+  unique_ptr<FairParRootFileIo> parInput1(new FairParRootFileIo());
+  parInput1->open(parFile.Data());
+  rtdb->setFirstInput(parInput1.release());
 
-    // Output file
-    TString outFile = "geane.cal.root";
+  unique_ptr<FairConstField> magField(new FairConstField());
+  magField->SetField      (0., 0., 20.); // values are in kG
+  magField->SetFieldRegion(-150, 150, -150, 150, -250, 250); // values are in cm (xmin,xmax,ymin,ymax,zmin,zmax)
+  run->SetField(magField.release());
 
-    // -----   Timer   --------------------------------------------------------
-    TStopwatch timer;
+  // -----   TorinoDetector hit producers   ---------------------------------
+  run->AddTask(new FairGeane());
+  run->AddTask(new FairTutGeaneTr());
 
-    // -----   Reconstruction run   -------------------------------------------
-    FairRunAna *fRun= new FairRunAna();
-    fRun->SetSource(new FairFileSource  (inFile));
-    fRun->SetSink  (new FairRootFileSink(outFile));
+  // -----   Initialize reconstruction run   --------------------------------
+  run->Init();
 
-    FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
-    FairParRootFileIo* parInput1 = new FairParRootFileIo();
-    parInput1->open(parFile.Data());
+  // -----   Run   ----------------------------------------------------------
+  TStopwatch timer;
+  timer.Start();
+  run->Run();
+  timer.Stop();
 
-    rtdb->setFirstInput(parInput1);
+  // -----   Summary  -------------------------------------------------------
+  cout << endl << endl;
 
-    FairConstField *fMagField=new FairConstField();
-    fMagField->SetField(0.,0.,20.); // values are in kG
-    fMagField->SetFieldRegion(-150, 150, -150, 150, -250, 250);// values are in cm (xmin,xmax,ymin,ymax,zmin,zmax)
-    fRun->SetField(fMagField);
-    
-    // -----   TorinoDetector hit  producers   ---------------------------------
-    FairGeane *Geane = new FairGeane();
-    fRun->AddTask(Geane);
+  // Extract the maximal used memory an add is as Dart measurement
+  // This line is filtered by CTest and the value send to CDash
+  FairSystemInfo sysInfo;
+  Float_t maxMemory=sysInfo.GetMaxMemory();
+  cout << "<DartMeasurement name=\"MaxMemory\" type=\"numeric/double\">";
+  cout << maxMemory;
+  cout << "</DartMeasurement>" << endl;
 
-    FairTutGeaneTr* geaneTask = new FairTutGeaneTr();
-    fRun->AddTask(geaneTask);
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
 
-    fRun->Init();
+  Float_t cpuUsage = ctime/rtime;
+  cout << "<DartMeasurement name=\"CpuLoad\" type=\"numeric/double\">";
+  cout << cpuUsage;
+  cout << "</DartMeasurement>" << endl;
 
-    timer.Start();
-    fRun->Run();
+  cout << endl << endl;
+  cout << "Output file is "    << outFile << endl;
+  cout << "Parameter file is " << parFile << endl;
+  cout << "Real time " << rtime << "s, CPU time "
+                       << ctime << "s" << endl << endl;
+  cout << "Macro finished successfully." << endl;
 
-    // -----   Finish   -------------------------------------------------------
-
-    cout << endl << endl;
-
-    // Extract the maximal used memory an add is as Dart measurement
-    // This line is filtered by CTest and the value send to CDash
-    FairSystemInfo sysInfo;
-    Float_t maxMemory=sysInfo.GetMaxMemory();
-    cout << "<DartMeasurement name=\"MaxMemory\" type=\"numeric/double\">";
-    cout << maxMemory;
-    cout << "</DartMeasurement>" << endl;
-
-    timer.Stop();
-    Double_t rtime = timer.RealTime();
-    Double_t ctime = timer.CpuTime();
-
-    Float_t cpuUsage=ctime/rtime;
-    cout << "<DartMeasurement name=\"CpuLoad\" type=\"numeric/double\">";
-    cout << cpuUsage;
-    cout << "</DartMeasurement>" << endl;
-
-    cout << endl << endl;
-    cout << "Output file is "    << outFile << endl;
-    cout << "Parameter file is " << parFile << endl;
-    cout << "Real time " << rtime << " s, CPU time " << ctime
-         << "s" << endl << endl;
-    cout << "Macro finished successfully." << endl;
-
-  // ------------------------------------------------------------------------
-    return 0;
+  return 0;
 }
