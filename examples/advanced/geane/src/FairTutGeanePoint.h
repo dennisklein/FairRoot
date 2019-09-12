6a58b2218baa2943adcc209d295c9a5d090632e0
Dennis Klein <d.klein@gsi.de>
2019-09-12 19:45:30 +0200

    Example.Geane: Cleanup FairTutGeanePoint
    * Apply CPPCoreGuidelines C.21, C.80, C.81
    * Forward declare TVector3
    * Use ClassDefOverride to silence compiler warnings
    * Drop unused #include <TObject.h>
    * Add missing #include <Rtypes.h>

--- examples/advanced/geane/src/FairTutGeanePoint.h
+++ examples/advanced/geane/src/FairTutGeanePoint.h
@@ -1,27 +1,30 @@
 /********************************************************************************
  *    Copyright (C) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
  *                                                                              *
- *              This software is distributed under the terms of the             * 
- *              GNU Lesser General Public Licence (LGPL) version 3,             *  
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
+
 #ifndef FAIRTUTGEANEPOINT_H
 #define FAIRTUTGEANEPOINT_H 1
 
+class TVector3;
 
 #include "FairMCPoint.h"
-
-#include "TObject.h"
-#include "TVector3.h"
+#include <Rtypes.h>
 
 class FairTutGeanePoint : public FairMCPoint
 {
-
   public:
-
-    /** Default constructor **/
-    FairTutGeanePoint();
-
+    // FIXME FairMCPoint should delete its copy ctor,
+    // then this whole section can be dropped
+    FairTutGeanePoint() = default;
+    FairTutGeanePoint(const FairTutGeanePoint&) = delete;
+    FairTutGeanePoint& operator=(const FairTutGeanePoint&) = delete;
+    FairTutGeanePoint(FairTutGeanePoint&&) = delete;
+    FairTutGeanePoint& operator=(FairTutGeanePoint&&) = delete;
+    ~FairTutGeanePoint() = default;
 
     /** Constructor with arguments
      *@param trackID  Index of MCTrack
@@ -33,24 +36,12 @@ class FairTutGeanePoint : public FairMCPoint
      *@param eLoss    Energy deposit [GeV]
      **/
     FairTutGeanePoint(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom,
-                     Double_t tof, Double_t length, Double_t eLoss);
-
-
-
-
-    /** Destructor **/
-    virtual ~FairTutGeanePoint();
+                      Double_t tof, Double_t length, Double_t eLoss);
 
     /** Output to screen **/
-    virtual void Print(const Option_t* opt) const;
+    void Print(const Option_t* opt) const override;
 
-  private:
-    /** Copy constructor **/
-    FairTutGeanePoint(const FairTutGeanePoint& point);
-    FairTutGeanePoint operator=(const FairTutGeanePoint& point);
-
-    ClassDef(FairTutGeanePoint,1)
-
+    ClassDefOverride(FairTutGeanePoint,1)
 };
 
 #endif
