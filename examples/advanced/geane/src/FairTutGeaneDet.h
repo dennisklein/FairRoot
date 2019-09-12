5b269117f04d2f90536859f99538fb71a9987ca3
Dennis Klein <d.klein@gsi.de>
2019-09-12 23:04:52 +0200

    Example.Geane: Cleanup FairTutGeaneDet
    * Apply CPPCoreGuidelines ES.47
    * Clarify ownership semantics
    * Fix memory leak
    * Add missing override
    * Use ClassDefOverride to silence compiler warnings
    * Use delegating ctor to deduplicate code
    * Commented out noop overrides
    * Remove ModifyGeometry

--- examples/advanced/geane/src/FairTutGeaneDet.h
+++ examples/advanced/geane/src/FairTutGeaneDet.h
@@ -1,89 +1,83 @@
 /********************************************************************************
  *    Copyright (C) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
  *                                                                              *
- *              This software is distributed under the terms of the             * 
- *              GNU Lesser General Public Licence (LGPL) version 3,             *  
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
+
 #ifndef FAIRTUTGEANEDET_H
 #define FAIRTUTGEANEDET_H
 
-#include "FairDetector.h"               // for FairDetector
-
-#include "Rtypes.h"                     // for Int_t, Double32_t, Double_t, etc
-#include "TLorentzVector.h"             // for TLorentzVector
-#include "TVector3.h"                   // for TVector3
 class FairTutGeanePoint;
 class FairVolume;
 class TClonesArray;
+class TVector3;
 
-class FairTutGeaneDet: public FairDetector
+#include "FairDetector.h"
+
+#include <Rtypes.h> // for Int_t, Double32_t, Double_t, etc
+#include <TLorentzVector.h>
+
+class FairTutGeaneDet : public FairDetector
 {
-
   public:
-
-    /**      Name :  Detector Name
-     *       Active: kTRUE for active detectors (ProcessHits() will be called)
-     *               kFALSE for inactive detectors
+    /** @arg Name Detector Name
+     *  @arg Active kTRUE for active detectors (ProcessHits() will be called)
+     *              kFALSE for inactive detectors
     */
     FairTutGeaneDet(const char* Name, Bool_t Active);
 
-    /**      default constructor    */
     FairTutGeaneDet();
+    FairTutGeaneDet(const FairTutGeaneDet&) = delete;
+    FairTutGeaneDet& operator=(const FairTutGeaneDet&) = delete;
+    FairTutGeaneDet(FairTutGeaneDet&&) = delete;
+    FairTutGeaneDet& operator=(FairTutGeaneDet&&) = delete;
+    ~FairTutGeaneDet();
 
-    /**       destructor     */
-    virtual ~FairTutGeaneDet();
+    /** Initialization of the detector is done here */
+    // void Initialize() override;
 
-    /**      Initialization of the detector is done here    */
-    virtual void   Initialize();
-
-    /**       this method is called for each step during simulation
-     *       (see FairMCApplication::Stepping())
+    /** this method is called for each step during simulation
+     *  (see FairMCApplication::Stepping())
     */
-    virtual Bool_t ProcessHits( FairVolume* v=0);
+    Bool_t ProcessHits(FairVolume* v = nullptr) override;
 
-    /**       Registers the produced collections in FAIRRootManager.     */
-    virtual void   Register();
+    /** Registers the produced collections in FairRootManager. */
+    void Register() override;
 
     /** Gets the produced collections */
-    virtual TClonesArray* GetCollection(Int_t iColl) const ;
+    TClonesArray* GetCollection(Int_t iColl) const override;
 
-    /**      has to be called after each event to reset the containers      */
-    virtual void   Reset();
+    /** has to be called after each event to reset the containers */
+    void Reset() override;
 
-    /**      Create the detector geometry        */
-    void ConstructGeometry();
+    /** Create the detector geometry */
+    void ConstructGeometry() override;
 
-    /**      Misalign the detector geometry        */
-    void ModifyGeometry();
-    void SetMisalignDetector(Bool_t tb=kTRUE) { fMisalignDetector = tb; }
-
-    /**      This method is an example of how to add your own point
-     *       of type FairTutGeaneDetPoint to the clones array
+    /** This method is an example of how to add your own point
+     *  of type FairTutGeaneDetPoint to the clones array
     */
     FairTutGeanePoint* AddHit(Int_t trackID, Int_t detID,
-                           TVector3 pos, TVector3 mom,
-                           Double_t time, Double_t length,
-                           Double_t eLoss);
+                              TVector3 pos, TVector3 mom,
+                              Double_t time, Double_t length,
+                              Double_t eLoss);
 
     /** The following methods can be implemented if you need to make
      *  any optional action in your detector during the transport.
     */
+    virtual void EndOfEvent() override;
 
-//    virtual void   CopyClones( TClonesArray* cl1,  TClonesArray* cl2 ,
-//                               Int_t offset) {;}
-    virtual void   SetSpecialPhysicsCuts() {;}
-    virtual void   EndOfEvent();
-    virtual void   FinishPrimary() {;}
-    virtual void   FinishRun() {;}
-    virtual void   BeginPrimary() {;}
-    virtual void   PostTrack() {;}
-    virtual void   PreTrack() {;}
-    virtual void   BeginEvent() {;}
-
+    // void CopyClones(TClonesArray* cl1, TClonesArray* cl2, Int_t offset) override {}
+    // void SetSpecialPhysicsCuts() override {}
+    // void FinishPrimary() override {}
+    // void FinishRun() override {}
+    // void BeginPrimary() override {}
+    // void PostTrack() override {}
+    // void PreTrack() override {}
+    // void BeginEvent() override {}
 
   private:
-
     /** Track information to be stored until the track leaves the
     active volume.
     */
@@ -96,14 +90,10 @@ class FairTutGeaneDet: public FairDetector
     Double32_t     fELoss;             //!  energy loss
 
     /** container for data points */
-
     Bool_t         fMisalignDetector;
     TClonesArray*  fFairTutGeanePointCollection;
 
-    FairTutGeaneDet(const FairTutGeaneDet&);
-    FairTutGeaneDet& operator=(const FairTutGeaneDet&);
-
-    ClassDef(FairTutGeaneDet,1)
+    ClassDefOverride(FairTutGeaneDet, 1)
 };
 
 #endif //FAIRTUTGEANEDET_H
